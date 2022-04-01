1d092402017996bf7ea79243546fc6a17f2838d1
Jestemrique <enrique.pastrana@gmail.com>
2022-04-01 17:02:04 +0200

    Added RestUtil.js

---
+++ util/RestUtil.js
@@ -0,0 +1,100 @@
+module.exports = class RestUtil {
+    constructor(requestUtil) {
+        this.request = requestUtil;
+    }
+    /**
+     * Make HTTP request to MicroStrategy REST API
+     * 
+     * @param {String} endpoint - example: 'auth/login'
+     * @param {Object}  params - request parameters
+     * @param {String} [method='GET'] HTTP method (GET/POST/PUT/DELETE/etc)
+     * @param {Object} customHeaders - optional: extra heaaders to merge into request
+     * @param {*} [advancedOptions={}]
+     * @returns {Promise} resolving with axios request response.
+     */
+    _makeRequest(...params) {
+        return this.request._makeRequest(...params);
+    }
+
+    /**
+     * Get stored auth token from memory. Assumes token was previously stored via auth.login() or setSessionHeaders()
+     * @returns {String} authToken
+     */
+    getAuthToken() {
+        return this.request.getAuthToken();
+    }
+
+    /**
+     * Store auth token in-memory for future executions. Other methods will automatically try to use this token.
+     * @param {String} tokenValue
+     */
+    setAuthToken(tokenValue) {
+        return this.request.setAuthToken(tokenValue);
+    }
+
+    /**
+     * @returns {Object} reusable credentials object for authentication request
+     */
+    getSessionCredentials() {
+        return this.request.getSessionCredentials();
+    }
+
+    /**
+     * Store object used to authenticate
+     */
+    setSessionCredentials(params) {
+        return this.request.setSessionCredentials(params);
+    }
+
+    /**
+     * @returns {Object} key:value headers required for authenticated requests
+     */
+    getSessionHeaders() {
+        return this.request.getSessionHeaders();
+    }
+
+    /**
+     * Store headers required to make authenticated requests. Leave blank to clear.
+     * @param {Object} sessionHeadersObject
+     */
+    setSessionHeaders(sessionHeadersObject) {
+        return this.request.setSessionHeaders(sessionHeadersObject);
+    }
+
+    /**
+     * Get header used to dictate which project to use.
+     * 
+     * @param {String} projectId
+     * @param {Object} [mergeHeaders={}] Optional, other headers to include in returned object.
+     * @returns {Object} containing header to specify this project in a request
+     */
+    getProjectHeader(...params) {
+        return this.request.getProjectHeader(...params);
+    }
+
+    throwIfFailed(result, successCode = 200, successField = 'data') {
+        const skipCodeCheck = this.request.skipValidateResponseStatusCode === true;
+        if (!skipCodeCheck && result.status != successCode) {
+            throw result;
+        }
+
+        if (successField) {
+            return result[successField];
+        }
+
+        return result;
+    }
+
+    /**
+     * Throw REST resonse as exception
+     * @param {Error} e - caught error from REST response
+     */
+    throwExceptionResponse(e) {
+        if (this.request.skipThrowOnHTTPEsception === true) {
+            return e;
+        }
+        throw (e && e.response && e.response.data) || e;
+    }
+
+
+};
