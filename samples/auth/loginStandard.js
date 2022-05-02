230512e1c4441f2786fa9f81536b49eb7a6e0b47
Enrique <enrique.pastrana@gmail.com>
2022-05-02 16:21:08 +0200

    auth samples

---
+++ samples/auth/loginStandard.js
@@ -0,0 +1,22 @@
+const mstr = require('../../lib/mstr.js');
+
+(async () => {
+  const baseUrl = 'http://10.23.3.162:8080/MicrostrategyLibrary/api';
+  const mstrApi = new mstr.REST({
+    baseUrl: baseUrl
+  });
+
+  const loginRequest = {
+      username: 'Administrator',
+      password: '',
+      loginMode: 1
+  };
+
+  const sessionInfo = await mstrApi.login(loginRequest);
+  console.log('session', sessionInfo);
+
+  //204 success
+  const logout = await mstrApi.logout();
+  console.log(logout.status);
+
+})();
