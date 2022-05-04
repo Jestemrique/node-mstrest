d94c8672db5e75f688e6a59bdb887a7d2cbd961e
Enrique <enrique.pastrana@gmail.com>
2022-05-04 12:35:52 +0200

    Adding cubes samples and fixing errors in cubes.js

--- samples/cubes/cubeSample.js
+++ samples/cubes/cubeSample.js
@@ -2,7 +2,7 @@ const mstr = require('../../lib/mstr.js');
 const convertStatusToString = require('./convertStatusToString');
 
 (async () => {
-  const baseUrl ) 'http://10.23.3.162:8080/MicroStrategyLibrary/api';
+  const baseUrl = 'http://10.23.3.162:8080/MicroStrategyLibrary/api';
   const mstrApi = new mstr.REST({
       baseUrl: baseUrl
   });
@@ -20,6 +20,32 @@ const convertStatusToString = require('./convertStatusToString');
   const cubeID = 'F3A8DE7844B308E8A13F7A90AD4C6544';
 
   const CubesAPI = mstrApi.cubes;
+
+  try{
+    const cubeStatus = await CubesAPI.getStatus(cubeID);
+    const states = convertStatusToString(cubeStatus);
+    
+    if (!states.ready){
+      await CubesAPI.publishCube(cubeID);
+    }
+    
+
+    //Begin test
+    //if (states.ready){
+    //  await CubesAPI.publishCube(cubeID);
+    //}
+    //End test
+
+    const instanceResponse = await CubesAPI.createCubeInstance(cubeID);
+    const instanceID = instanceResponse.instanceId;
+
+    const cubeData = await CubesAPI.getInstanceResults(cubeID, instanceID);
+    console.log(cubeData);
+  } catch (e) {
+    console.log(e);
+  }
+
+  await mstrApi.logout();
   
 
 })();
